ad95805c4862a1fb935de61832aa0902ad4ab790
Haruki <127585577+Aaaakkkkbot@users.noreply.github.com>
2025-01-11 00:00:33 +0530

    Add files via upload

--- helper/database.py
+++ helper/database.py
@@ -1,120 +1,92 @@
-import pymongo
-import os
-from helper.date import add_date
-from config import *
-mongo = pymongo.MongoClient(DB_URL)
-db = mongo[DB_NAME]
-dbcol = db["user"]
+#(©)CodeXBotz
+#recoded by @Its_Oreki_Hotarou
 
-# Total User
+import asyncio
+import pymongo, os
+from config import DB_URI, DB_NAME
 
 
-def total_user():
-    user = dbcol.count_documents({})
-    return user
+dbclient = pymongo.MongoClient(DB_URI)
+database = dbclient[DB_NAME]
 
-# insert bot Data
-
-
-def botdata(chat_id):
-    bot_id = int(chat_id)
-    try:
-        bot_data = {"_id": bot_id, "total_rename": 0, "total_size": 0}
-        dbcol.insert_one(bot_data)
-    except:
-        pass
+# Database collections
+admin_data = database['admins']
+user_data = database['users']
+premium_user = database['premium']
 
 
-def total_rename(chat_id, renamed_file):
-    now = int(renamed_file) + 1
-    dbcol.update_one({"_id": chat_id}, {"$set": {"total_rename": str(now)}})
+# User functions
+async def present_user(user_id: int):
+    found = user_data.find_one({'_id': user_id})
+    return bool(found)
 
 
-def total_size(chat_id, total_size, now_file_size):
-    now = int(total_size) + now_file_size
-    dbcol.update_one({"_id": chat_id}, {"$set": {"total_size": str(now)}})
+async def add_user(user_id: int):
+    user_data.insert_one({'_id': user_id})
+    return
 
 
-# insert user data
-def insert(chat_id):
-    user_id = int(chat_id)
-    user_det = {"_id": user_id, "file_id": None, "caption": None, "daily": 0, "date": 0,
-                "uploadlimit": 5368709120, "used_limit": 0, "usertype": "Free", "prexdate": None}
-    try:
-        dbcol.insert_one(user_det)
-    except:
-        return True
-        pass
+async def full_userbase():
+    user_docs = user_data.find()
+    user_ids = []
+    for doc in user_docs:
+        user_ids.append(doc['_id'])
+
+    return user_ids
 
 
-def addthumb(chat_id, file_id):
-    dbcol.update_one({"_id": chat_id}, {"$set": {"file_id": file_id}})
+async def del_user(user_id: int):
+    user_data.delete_one({'_id': user_id})
+    return
 
 
-def delthumb(chat_id):
-    dbcol.update_one({"_id": chat_id}, {"$set": {"file_id": None}})
+# Premium user functions
+async def is_premium(user_id: int):
+    found = premium_user.find_one({'_id': user_id})
+    return bool(found)
 
 
-def addcaption(chat_id, caption):
-    dbcol.update_one({"_id": chat_id}, {"$set": {"caption": caption}})
+async def add_premium(user_id: int):
+    premium_user.insert_one({'_id': user_id})
+    return
 
 
-def delcaption(chat_id):
-    dbcol.update_one({"_id": chat_id}, {"$set": {"caption": None}})
+async def get_premium_users():
+    premium_docs = premium_user.find()
+    premium_ids = []
+    for doc in premium_docs:
+        premium_ids.append(doc['_id'])
+
+    return premium_ids
 
 
-def dateupdate(chat_id, date):
-    dbcol.update_one({"_id": chat_id}, {"$set": {"date": date}})
+async def remove_premium(user_id: int):
+    premium_user.delete_one({'_id': user_id})
+    return
 
+# Function to check if an admin exists
+async def present_admin(admin_id: int):
+    loop = asyncio.get_running_loop()
+    found = await loop.run_in_executor(None, lambda: admin_data.find_one({'_id': admin_id}))
+    return bool(found)
 
-def used_limit(chat_id, used):
-    dbcol.update_one({"_id": chat_id}, {"$set": {"used_limit": used}})
+# Function to add a new admin
+async def add_admin(admin_id: int):
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, lambda: admin_data.insert_one({'_id': admin_id}))
+    return
 
+# Function to retrieve all admins
+async def full_adminbase():
+    loop = asyncio.get_running_loop()
+    admin_docs = await loop.run_in_executor(None, lambda: admin_data.find())
+    admin_ids = []
+    for doc in admin_docs:
+        admin_ids.append(doc['_id'])
+    return admin_ids
 
-def usertype(chat_id, type):
-    dbcol.update_one({"_id": chat_id}, {"$set": {"usertype": type}})
-
-
-def uploadlimit(chat_id, limit):
-    dbcol.update_one({"_id": chat_id}, {"$set": {"uploadlimit": limit}})
-
-
-def addpre(chat_id):
-    date = add_date()
-    dbcol.update_one({"_id": chat_id}, {"$set": {"prexdate": date[0]}})
-
-
-def addpredata(chat_id):
-    dbcol.update_one({"_id": chat_id}, {"$set": {"prexdate": None}})
-
-
-def daily(chat_id, date):
-    dbcol.update_one({"_id": chat_id}, {"$set": {"daily": date}})
-
-
-def find(chat_id):
-    id = {"_id": chat_id}
-    x = dbcol.find(id)
-    for i in x:
-        file = i["file_id"]
-        try:
-            caption = i["caption"]
-        except:
-            caption = None
-
-        return [file, caption]
-
-
-def getid():
-    values = []
-    for key in dbcol.find():
-        id = key["_id"]
-        values.append((id))
-    return values
-
-def delete(id):
-    dbcol.delete_one(id)
-
-
-def find_one(id):
-    return dbcol.find_one({"_id": id})
+# Function to delete an admin
+async def del_admin(admin_id: int):
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, lambda: admin_data.delete_one({'_id': admin_id}))
+    return
